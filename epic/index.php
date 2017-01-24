f63adda964427150faa2238ea9e033ca6540f860
Benjamin Smith <rios7467@gmail.com>
2017-01-24 16:58:17 -0700

    Create index.php

---
+++ epic/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>teamSafeHouse</title>
+	</head>
+	<body>
+		<header>
+			Team Safe House
+		</header>
+		<main>
+			<h2>System Summary </h2>
+			<p>Desktop and mobile website for home buyers that want to do more research into the neighborhoods where they are potentially moving.  They can view information about various crimes and/ or proximity to parks in one place. </p>
+			<h2>User Stories</h2>
+			<p>user story:
+				as a user, I want to explore neighborhoods with a low crime rate
+				as a user, I want to find what the crime rate is in the neighborhood i live in
+				as a user, I want to find parks in different neighborhoods
+				as a user, I want to find parks in the neighborhood I already live in</p>
+		</main>
+	</body>
+</html>
